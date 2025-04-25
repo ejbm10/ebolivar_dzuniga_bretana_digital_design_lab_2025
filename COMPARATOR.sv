ece8e72ba9c93b6503cefccb11db38fc1e5935b6
ejbm10 <eduardo.bm10@estudiantec.cr>
2025-04-24 22:41:26 -0600

    Modified FSM and new .gitignore

--- COMPARATOR.sv
+++
@@ -1,9 +0,0 @@
-module COMPARATOR (
-	input logic [25:0] A,
-	input logic [25:0] B,
-	output logic cmp
-);
-
-	assign cmp = (A == B);
-	
-endmodule
--- FSM.sv
+++ FSM.sv
@@ -1,18 +1,19 @@
 module FSM (
 	input logic clk,
 	input logic rst,
-	input logic s,
-	input logic fpga_move,
-	input logic arduino_move,
-	input logic t_out_fpga,
-	input logic t_out_arduino,
-	input logic fpga_winner,
-	input logic arduino_winner,
-	input logic full_board_fpga,
-	input logic full_board_arduino,
-	output logic en_fpga_loading,
-	output logic en_arduino_loading,
-	output logic start_timer,
+	input logic select,
+	input logic move,
+	input logic time_out,
+	input logic win,
+	input logic full,
+	output logic rst_timer,
+	output logic en_neg,
+	output logic en_load,
+	output logic en_check,
+	output logic en_comp_board,
+	output logic player1_win,
+	output logic player2_win,
+	output logic game_over
 );
 
 	logic [3:0] state, next_state;
@@ -24,26 +25,27 @@ module FSM (
 	
 	always_comb begin
 		case (state)
-			4'b0000: next_state = s ? 4'b0001 : 4'b0010;
-			4'b0001: next_state = load_fpga ? 4'b0101 : 4'b0011;
-			4'b0010: next_state = load_arduino ? 4'b0110 : 4'b0100;
-			4'b0011: next_state = t_out_fpga ? 4'b0010 : 4'b0001;
-			4'b0100: next_state = t_out_arduino ? 4'b0001 : 4'b0010;
-			4'b0101: next_state = 4'b0111;
-			4'b0110: next_state = 4'b1000;
-			4'b0111: next_state = fpga_winner ? 4'b1011 : 4'b1001;
-			4'b1000: next_state = arduino_winner ? 4'b1100 : 4'b1010;
-			4'b1001: next_state = full_board_fpga ? 4'b1101 : 4'b0010;
-			4'b1010: next_state = full_board_arduino ? 4'b1101 : 4'b0001;
-			4'b1011: next_state = 4'b1011;
-			4'b1100: next_state = 4'b1100;
-			4'b1101: next_state = 4'b1101;
+			4'b0000: next_state = select ? 4'b0001 : 4'b0010;
+			4'b0001: next_state = move ? 4'b0101 : 4'b0011;
+			4'b0010: next_state = move ? 4'b0101 : 4'b0011;
+			4'b0011: next_state = time_out ? 4'b0100 : 4'b0000;
+			4'b0100: next_state = 4'b0000;
+			4'b0101: next_state = 4'b0110;
+			4'b0110: next_state = win ? (select ? 4'b1000 : 4'b1001) : 4'b0111;
+			4'b0111: next_state = full ? 4'b1010 : 4'b0100;
+			4'b1000: next_state = 4'b1000;
+			4'b1001: next_state = 4'b1001;
+			4'b1010: next_state = 4'b1010;
 		endcase
 	end
 	
-	assign en_fpga_loading = (state == 4'b0101);
-	assign en_arduino_loading = (state == 4'b0110);
-	assign en_timer = (state == 4'b0001 | state == 4'b0010);
-	
+	assign rst_timer = (state == 4'b0100);
+	assign en_neg = (state == 4'b0100);
+	assign en_load = (state == 4'b0101);
+	assign en_check = (state == 4'b0110);
+	assign en_comp_board = (state == 4'b0111);
+	assign player1_win = (state == 4'b1000);
+	assign player2_win = (state == 4'b1001);
+	assign game_over = (state == 4'b1010);
 	
 endmodule
