2bfc3a75ce9f131ef759a3326bcb5ad0690b4dc9
ejbm10 <eduardo.bm10@estudiantec.cr>
2025-05-02 22:32:00 -0600

    check columns and rows

---
+++ CheckRowWin.sv
@@ -0,0 +1,48 @@
+module CheckRowWin (
+	input logic [1:0] val0,
+	input logic [1:0] val1,
+	input logic [1:0] val2,
+	input logic [1:0] val3,
+	input logic [1:0] val4,
+	input logic [1:0] val5,
+	input logic [1:0] val6,
+	output logic win
+);
+
+	logic win1, win2, win3, win4;
+	
+	Check4InLine op1 (
+		.q0(val0),
+		.q1(val1),
+		.q2(val2),
+		.q3(val3),
+		.win(win1)
+	);
+	
+	Check4InLine op2 (
+		.q0(val1),
+		.q1(val2),
+		.q2(val3),
+		.q3(val4),
+		.win(win2)
+	);
+	
+	Check4InLine op3 (
+		.q0(val2),
+		.q1(val3),
+		.q2(val4),
+		.q3(val5),
+		.win(win3)
+	);
+	
+	Check4InLine op4 (
+		.q0(val3),
+		.q1(val4),
+		.q2(val5),
+		.q3(val6),
+		.win(win4)
+	);
+	
+	assign win = win1 | win2 | win3 | win4;
+	
+endmodule
--- VictoryValidator.sv
+++ VictoryValidator.sv
@@ -9,63 +9,148 @@ module VictoryValidator (
 	output logic win
 );
 	
-	logic col0_win, col0_win1, col0_win2, col0_win3;
-	logic col1_win, col1_win1, col1_win2, col1_win3;
+	logic col_win, col0_win, col1_win, col2_win, col3_win, col4_win, col5_win, col6_win;
+	logic row_win, row0_win, row1_win, row2_win, row3_win, row4_win, row5_win;
 	
-	// Columna 0
-	Check4InLine op1_column0 (
-		.q0(val00),
-		.q1(val01),
-		.q2(val02),
-		.q3(val03),
-		.win(col0_win1)
+	CheckColumnWin col0 (
+		.val0(val00),
+		.val1(val01),
+		.val2(val02),
+		.val3(val03),
+		.val4(val04),
+		.val5(val05),
+		.win(col0_win)
 	);
 	
-	Check4InLine op2_column0 (
-		.q0(val01),
-		.q1(val02),
-		.q2(val03),
-		.q3(val04),
-		.win(col0_win2)
+	CheckColumnWin col1 (
+		.val0(val10),
+		.val1(val11),
+		.val2(val12),
+		.val3(val13),
+		.val4(val14),
+		.val5(val15),
+		.win(col1_win)
 	);
 	
-	Check4InLine op3_column0 (
-		.q0(val02),
-		.q1(val03),
-		.q2(val04),
-		.q3(val05),
-		.win(col0_win3)
+	CheckColumnWin col2 (
+		.val0(val20),
+		.val1(val21),
+		.val2(val22),
+		.val3(val23),
+		.val4(val24),
+		.val5(val25),
+		.win(col2_win)
 	);
 	
-	// Columna 1
-	Check4InLine op1_column1 (
-		.q0(val10),
-		.q1(val11),
-		.q2(val12),
-		.q3(val13),
-		.win(col1_win1)
+	CheckColumnWin col3 (
+		.val0(val30),
+		.val1(val31),
+		.val2(val32),
+		.val3(val33),
+		.val4(val34),
+		.val5(val35),
+		.win(col3_win)
 	);
 	
-	Check4InLine op2_column1 (
-		.q0(val11),
-		.q1(val12),
-		.q2(val13),
-		.q3(val14),
-		.win(col1_win2)
+	CheckColumnWin col4 (
+		.val0(val40),
+		.val1(val41),
+		.val2(val42),
+		.val3(val43),
+		.val4(val44),
+		.val5(val45),
+		.win(col4_win)
 	);
 	
-	Check4InLine op3_column1 (
-		.q0(val12),
-		.q1(val13),
-		.q2(val14),
-		.q3(val15),
-		.win(col1_win3)
+	CheckColumnWin col5 (
+		.val0(val50),
+		.val1(val51),
+		.val2(val52),
+		.val3(val53),
+		.val4(val54),
+		.val5(val55),
+		.win(col5_win)
 	);
 	
-	assign col0_win = col0_win1 | col0_win2 | col0_win3;
-	assign col1_win = col1_win1 | col1_win2 | col1_win3;
+	CheckColumnWin col6 (
+		.val0(val60),
+		.val1(val61),
+		.val2(val62),
+		.val3(val63),
+		.val4(val64),
+		.val5(val65),
+		.win(col6_win)
+	);
 	
-	assign win = col0_win | col1_win;
+	CheckRowWin row0 (
+		.val0(val00),
+		.val1(val10),
+		.val2(val20),
+		.val3(val30),
+		.val4(val40),
+		.val5(val50),
+		.val6(val60),
+		.win(row0_win)
+	);
 	
+	CheckRowWin row1 (
+		.val0(val01),
+		.val1(val11),
+		.val2(val21),
+		.val3(val31),
+		.val4(val41),
+		.val5(val51),
+		.val6(val61),
+		.win(row1_win)
+	);
+	
+	CheckRowWin row2 (
+		.val0(val02),
+		.val1(val12),
+		.val2(val22),
+		.val3(val32),
+		.val4(val42),
+		.val5(val52),
+		.val6(val62),
+		.win(row2_win)
+	);
+	
+	CheckRowWin row3 (
+		.val0(val03),
+		.val1(val13),
+		.val2(val23),
+		.val3(val33),
+		.val4(val43),
+		.val5(val53),
+		.val6(val63),
+		.win(row3_win)
+	);
+	
+	CheckRowWin row4 (
+		.val0(val04),
+		.val1(val14),
+		.val2(val24),
+		.val3(val34),
+		.val4(val44),
+		.val5(val54),
+		.val6(val64),
+		.win(row4_win)
+	);
+	
+	CheckRowWin row5 (
+		.val0(val05),
+		.val1(val15),
+		.val2(val25),
+		.val3(val35),
+		.val4(val45),
+		.val5(val55),
+		.val6(val65),
+		.win(row5_win)
+	);
+	
+	assign col_win = col0_win | col1_win | col2_win | col3_win | col4_win | col5_win | col6_win;
+	assign row_win = row0_win | row1_win | row2_win | row3_win | row4_win | row5_win;
+	
+	assign win = col_win | row_win;
 endmodule
 
